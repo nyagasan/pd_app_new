8b426a863ecfa9d6b1ee4931aa2a50c7a0753082
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-12-19 04:36:29 +0900

    refact: 交換商品カードのリファクト

--- lib/pages/exchange_page.dart
+++ lib/pages/exchange_page.dart
@@ -6,39 +6,41 @@ class ExchangePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        Padding(
-          //一番上
-          padding: const EdgeInsets.only(top: 35, right: 20, left: 20),
-          child: Container(
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 239, 238, 238),
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.white,
-                  offset: Offset(-5, -5),
-                  blurRadius: 15,
-                ),
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.3),
-                  offset: Offset(5, 5),
-                  blurRadius: 15,
-                ),
-              ],
-            ),
-            height: 200,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: 200,
-                  height: 200,
-                  color: Colors.black,
-                ),
-                Expanded(
+        // 交換商品カード
+        Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 239, 238, 238),
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white,
+                offset: Offset(-5, -5),
+                blurRadius: 15,
+              ),
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                offset: Offset(5, 5),
+                blurRadius: 15,
+              ),
+            ],
+          ),
+          height: 200,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // 商品画像
+              Image.asset(
+                'assets/images/exc_item5.png',
+                width: 200,
+                height: 200,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
+                      // 交換店舗名
                       Text('Yショップ', style: TextStyle(fontSize: 18)),
                       Container(
                         //下線
@@ -46,22 +48,29 @@ class ExchangePage extends StatelessWidget {
                         width: 100,
                         color: Colors.black,
                       ),
-                      SizedBox(height: 5),
-                      Text('ハーゲンダッツあふれるショコラ抹茶＆クッキー',
-                          style: TextStyle(fontSize: 20)),
-                      SizedBox(height: 16),
+                      Spacer(),
+                      // 商品名
+                      Text(
+                        'ハーゲンダッツあふれるショコラ抹茶＆クッキー',
+                        style: TextStyle(fontSize: 20),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 3,
+                      ),
+                      Spacer(),
                       ElevatedButton(
                         onPressed: () {},
-                        child: Text('交換',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                            )),
+                        child: Expanded(
+                          child: Text('交換',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              )),
+                        ),
                         style: ElevatedButton.styleFrom(
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(8),
                           ),
-                          minimumSize: Size(160, 50), //ボタンの幅と高さ
+                          minimumSize: Size(300, 45), //ボタンの幅と高さ
                           backgroundColor:
                               const Color.fromARGB(255, 210, 15, 1),
                         ),
@@ -69,8 +78,83 @@ class ExchangePage extends StatelessWidget {
                     ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 239, 238, 238),
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white,
+                offset: Offset(-5, -5),
+                blurRadius: 15,
+              ),
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                offset: Offset(5, 5),
+                blurRadius: 15,
+              ),
+            ],
+          ),
+          height: 200,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // 商品画像
+              Image.asset(
+                'assets/images/exc_item5.png',
+                width: 200,
+                height: 200,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // 交換店舗名
+                      Text('Yショップ', style: TextStyle(fontSize: 18)),
+                      Container(
+                        //下線
+                        height: 1,
+                        width: 100,
+                        color: Colors.black,
+                      ),
+                      Spacer(),
+                      // 商品名
+                      Text(
+                        'ハーゲンダッツあふれるショコラ抹茶＆クッキー',
+                        style: TextStyle(fontSize: 20),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 3,
+                      ),
+                      Spacer(),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Expanded(
+                          child: Text('交換',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              )),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          minimumSize: Size(300, 45), //ボタンの幅と高さ
+                          backgroundColor:
+                              const Color.fromARGB(255, 210, 15, 1),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
         Padding(
