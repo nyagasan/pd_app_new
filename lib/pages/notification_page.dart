6a1fe7117223ad45e469e29e8eadcec45e706a78
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-12-19 15:35:53 +0900

    refact: NotificationPageをリファクト

--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -9,8 +9,9 @@ class NotificationPage extends StatelessWidget {
       appBar: RedAppBar('お知らせ'),
       body: ListView(
         children: [
+          // お知らせカード
           Padding(
-            padding: const EdgeInsets.only(),
+            padding: const EdgeInsets.all(8.0),
             child: Container(
               decoration: BoxDecoration(
                 color: const Color.fromARGB(255, 255, 255, 255),
@@ -27,125 +28,59 @@ class NotificationPage extends StatelessWidget {
                   ),
                 ],
               ),
-              height: 80,
-              width: 300,
-              child:
-                  Center(child: Text('お知らせ', style: TextStyle(fontSize: 25))),
-            ),
-          ),
-          Padding(
-            //一つ目
-            padding: const EdgeInsets.only(top: 40, right: 20, left: 20),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 255, 255, 255),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-5, -5),
-                    blurRadius: 15,
-                  ),
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    offset: Offset(5, 5),
-                    blurRadius: 15,
-                  ),
-                ],
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          '2023 10/02',
+                          style: TextStyle(
+                            color: Color(0xFF444444),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        // 店舗名
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          decoration: ShapeDecoration(
+                            color: Color(0xFFD32F2F),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                          ),
+                          child: Text(
+                            'LA TERRA',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Text(
+                      'KITランチの価格改定',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      '10/2より、商品価格の改定を行います。ああああああああああああああああああああああああああああああああ',
+                      style: TextStyle(
+                        color: Color(0xFF444444),
+                        fontSize: 16,
+                      ),
+                    )
+                  ],
+                ),
               ),
-              height: 100,
-              child: Center(child: Text('aaaaaa')),
-            ),
-          ),
-          Padding(
-            //二つ目
-            padding: const EdgeInsets.only(top: 40, right: 20, left: 20),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 255, 255, 255),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-5, -5),
-                    blurRadius: 15,
-                  ),
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    offset: Offset(5, 5),
-                    blurRadius: 15,
-                  ),
-                ],
-              ),
-              height: 100,
-              child: Center(child: Text('aaaaaa')),
-            ),
-          ),
-          Padding(
-            //三つ目
-            padding: const EdgeInsets.only(top: 40, right: 20, left: 20),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 255, 255, 255),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-5, -5),
-                    blurRadius: 15,
-                  ),
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    offset: Offset(5, 5),
-                    blurRadius: 15,
-                  ),
-                ],
-              ),
-              height: 100,
-              child: Center(child: Text('aaaaaa')),
-            ),
-          ),
-          Padding(
-            //四つ目
-            padding: const EdgeInsets.only(top: 40, right: 20, left: 20),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 255, 255, 255),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-5, -5),
-                    blurRadius: 15,
-                  ),
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    offset: Offset(5, 5),
-                    blurRadius: 15,
-                  ),
-                ],
-              ),
-              height: 100,
-              child: Center(child: Text('aaaaaa')),
-            ),
-          ),
-          Padding(
-            //五つ目
-            padding: const EdgeInsets.only(top: 40, right: 20, left: 20),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 255, 255, 255),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-5, -5),
-                    blurRadius: 15,
-                  ),
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    offset: Offset(5, 5),
-                    blurRadius: 15,
-                  ),
-                ],
-              ),
-              height: 100,
-              child: Center(child: Text('aaaaaa')),
             ),
           ),
         ],
