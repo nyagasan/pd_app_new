2f5c873c25c7291da5ac82237f07e2335ede46c5
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-12-19 15:45:58 +0900

    feat: notificationCardを実装

---
+++ lib/widgets/notification_card.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class NotificationCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 255, 255, 255),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white,
+              offset: Offset(-5, -5),
+              blurRadius: 15,
+            ),
+            BoxShadow(
+              color: Colors.black.withOpacity(0.3),
+              offset: Offset(5, 5),
+              blurRadius: 15,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    '2023 10/02',
+                    style: TextStyle(
+                      color: Color(0xFF444444),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  // 店舗名
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    decoration: ShapeDecoration(
+                      color: Color(0xFFD32F2F),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                    ),
+                    child: Text(
+                      'LA TERRA',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Text(
+                'KITランチの価格改定',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                '10/2より、商品価格の改定を行います。ああああああああああああああああああああああああああああああああ',
+                style: TextStyle(
+                  color: Color(0xFF444444),
+                  fontSize: 16,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
