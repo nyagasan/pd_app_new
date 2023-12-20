34fb8916b9969c487d150851dab6fee928935e2b
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-12-20 23:54:55 +0900

    feat: 営業時間表示を実装中

--- lib/pages/store_hours_page.dart
+++ lib/pages/store_hours_page.dart
@@ -1,104 +1,15 @@
 import 'package:flutter/material.dart';
+import '../widgets/store_card.dart';
 
 class StoreHoursPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: Color.fromARGB(255, 240, 240, 240),
       child: ListView(
+        padding: EdgeInsets.all(20),
         children: [
-          Padding(
-              padding: const EdgeInsets.only(
-            top: 20,
-          )),
-          Text('対応店舗営業時間',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30,
-              )),
-          Padding(
-              padding: const EdgeInsets.only(
-            top: 10,
-          )),
-          Row(
-            //下線
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 1,
-                width: 300,
-                color: Colors.black,
-              ),
-            ],
-          ),
-          Padding(
-            //一番上
-            padding: const EdgeInsets.only(top: 40, right: 20, left: 20),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 255, 255, 255),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-5, -5),
-                    blurRadius: 15,
-                  ),
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    offset: Offset(5, 5),
-                    blurRadius: 15,
-                  ),
-                ],
-              ),
-              height: 170,
-              child: Center(child: Text('aaaaaa')),
-            ),
-          ),
-          Padding(
-            //真ん中
-            padding: const EdgeInsets.only(top: 40, right: 20, left: 20),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 255, 255, 255),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-5, -5),
-                    blurRadius: 15,
-                  ),
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    offset: Offset(5, 5),
-                    blurRadius: 15,
-                  ),
-                ],
-              ),
-              height: 170,
-              child: Center(child: Text('aaaaaa')),
-            ),
-          ),
-          Padding(
-            //一番下
-            padding: const EdgeInsets.only(top: 40, right: 20, left: 20),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 255, 255, 255),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-5, -5),
-                    blurRadius: 15,
-                  ),
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    offset: Offset(5, 5),
-                    blurRadius: 15,
-                  ),
-                ],
-              ),
-              height: 170,
-              child: Center(child: Text('aaaaaa')),
-            ),
-          ),
+          StoreCard(),
         ],
       ),
     );
---
+++ lib/widgets/store_card.dart
@@ -0,0 +1,118 @@
+import 'dart:isolate';
+
+import 'package:flutter/material.dart';
+import '../model/store_model.dart';
+
+class StoreCard extends StatelessWidget {
+  // final StoreData storeData;
+  
+
+  // StoreCard ({required this.storeData});
+
+  @override
+  Widget build(BuildContext) {
+    // bool isOparationgNow = storeData.isOperatingNow();
+
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 255, 255, 255),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.white,
+            offset: Offset(-5, -5),
+            blurRadius: 15,
+          ),
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            offset: Offset(5, 5),
+            blurRadius: 15,
+          ),
+        ],
+      ),
+      // 全体、Flex row
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          FittedBox(
+            fit: BoxFit.cover,
+            child: Container(
+              width: 10,
+              height: 150,
+              color: Color.fromARGB(255, 210, 15, 1),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 14.0),
+            child: Column(
+              children: [
+                Icon(
+                  Icons.restaurant,
+                  size: 50,
+                  color: Color.fromARGB(255, 210, 15, 1),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  decoration: ShapeDecoration(
+                    color: Color(0xFFD32F2F),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                  ),
+                  child: Text(
+                    '営業中',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              Text(
+                '21号館 1F ガクショク',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              // 店舗名
+              Text(
+                'LA TERRA',
+                style: TextStyle(
+                  color: Color(0xFF131212),
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 12),
+              Text(
+                '営業時間：10:00~15:00',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'HPリンク：example.com',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
