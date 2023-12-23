3df34c8f912d00e3613edf7ccd51c34f92983ffc
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-12-23 23:31:44 +0900

    feat: 2個目のCardを追加

--- lib/widgets/store_card.dart
+++ lib/widgets/store_card.dart
@@ -30,84 +30,173 @@ class StoreCard extends StatelessWidget {
         ],
       ),
       // 全体、Flex row
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
+      child: Column(
         children: [
-          FittedBox(
-            fit: BoxFit.cover,
-            child: Container(
-              width: 10,
-              height: 150,
-              color: Color.fromARGB(255, 210, 15, 1),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 14.0),
-            child: Column(
-              children: [
-                Icon(
-                  Icons.restaurant,
-                  size: 50,
+          // Active要素
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              FittedBox(
+                fit: BoxFit.cover,
+                child: Container(
+                  width: 10,
+                  height: 150,
                   color: Color.fromARGB(255, 210, 15, 1),
                 ),
-                SizedBox(height: 10),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  decoration: ShapeDecoration(
-                    color: Color(0xFFD32F2F),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(100),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.restaurant,
+                      size: 50,
+                      color: Color.fromARGB(255, 210, 15, 1),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      decoration: ShapeDecoration(
+                        color: Color(0xFFD32F2F),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                      child: Text(
+                        '営業中',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Text(
+                    '21号館 1F ガクショク',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
                     ),
                   ),
-                  child: Text(
-                    '営業中',
+                  // 店舗名
+                  Text(
+                    'LA TERRA',
                     style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 16),
+                      color: Color(0xFF131212),
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ),
-              ],
-            ),
+                  const SizedBox(height: 12),
+                  Text(
+                    '営業時間：10:00~15:00',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'HPリンク：example.com',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-          Column(
+          // Disabled要素
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text(
-                '21号館 1F ガクショク',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w400,
+              FittedBox(
+                fit: BoxFit.cover,
+                child: Container(
+                  width: 10,
+                  height: 150,
+                  color: Color.fromARGB(255, 210, 15, 1),
                 ),
               ),
-              // 店舗名
-              Text(
-                'LA TERRA',
-                style: TextStyle(
-                  color: Color(0xFF131212),
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.restaurant,
+                      size: 50,
+                      color: Color.fromARGB(255, 210, 15, 1),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      decoration: ShapeDecoration(
+                        color: Color(0xFFD32F2F),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                      child: Text(
+                        '営業中',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 12),
-              Text(
-                '営業時間：10:00~15:00',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                'HPリンク：example.com',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w400,
-                ),
+              Column(
+                children: [
+                  Text(
+                    '21号館 1F ガクショク',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  // 店舗名
+                  Text(
+                    'LA TERRA',
+                    style: TextStyle(
+                      color: Color(0xFF131212),
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(
+                    '営業時間：10:00~15:00',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'HPリンク：example.com',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
