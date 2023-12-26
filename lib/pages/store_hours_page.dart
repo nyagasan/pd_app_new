d057a9f6160351219679544a50e201b525822114
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-12-26 14:03:23 +0900

    feat: モックを実装

--- lib/pages/store_hours_page.dart
+++ lib/pages/store_hours_page.dart
@@ -9,7 +9,33 @@ class StoreHoursPage extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.all(20),
         children: [
-          StoreCard(),
+          StoreCard(
+              storeName: 'LA TERRA',
+              storeLocation: '21号館1F ガクショク',
+              storeURL: 'example.com',
+              storeOpenTime: '10:00~15:00',
+              storeOperatingNow: true),
+          SizedBox(height: 20),
+          StoreCard(
+              storeName: 'Yショップ アクア店',
+              storeLocation: '27号館2F',
+              storeURL: 'example.com',
+              storeOpenTime: '8:30~19:00',
+              storeOperatingNow: true),
+          SizedBox(height: 20),
+          StoreCard(
+              storeName: 'KITブックセンター',
+              storeLocation: '21号館2F',
+              storeURL: 'example.com',
+              storeOpenTime: '8:30~17:00',
+              storeOperatingNow: true),
+          SizedBox(height: 20),
+          StoreCard(
+              storeName: 'ENERGY',
+              storeLocation: 'やつかほキャンパス 62号館',
+              storeURL: 'example.com',
+              storeOpenTime: '11:00~15:00',
+              storeOperatingNow: false),
         ],
       ),
     );
